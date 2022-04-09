10341a83341d1882df0b4d69d11fbfd2ed1e72a7
Aman Thakur <builtbyaman@gmail.com>
2022-04-09 14:10:00 +0530

    added context

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { NewItemFormContainer, NewItemButton, NewItemInput } from './styles';
+import { useFocus } from './utils/useFocus';
 
 type NewItemFormProps = {
   onAdd(text: string): void;
@@ -7,11 +8,21 @@ type NewItemFormProps = {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('');
+  const inputRef = useFocus();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onAdd(text);
+  };
 
   return (
-    <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
-      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    <NewItemFormContainer onSubmit={handleSubmit}>
+      <NewItemInput
+        value={text}
+        ref={inputRef}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <NewItemButton type="submit">Create</NewItemButton>
     </NewItemFormContainer>
   );
 };
--- src/styles.ts
+++ src/styles.ts
@@ -57,7 +57,7 @@ export const AddItemButton = styled.button<AddItemButtonProps>`
   }
 `;
 
-export const NewItemFormContainer = styled.div`
+export const NewItemFormContainer = styled.form`
   max-width: 300px;
   display: flex;
   flex-direction: column;
@@ -78,7 +78,7 @@ export const NewItemButton = styled.button`
 export const NewItemInput = styled.input`
   border-radius: 3px;
   border: none;
-  box-shadow: #091240 0px 1px 0 0;
+  box-shadow: #091e4240 0px 1px 0 0;
   margin-bottom: 0.5rem;
   padding: 0.5rem 1rem;
   width: 100%;
